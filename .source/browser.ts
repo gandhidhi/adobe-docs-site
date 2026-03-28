8aa250a0999a36ddb6945fec0215cfaf9456d3dd
gandhidhi <yu-suzuki@kua.kyoto-art.ac.jp>
2026-03-28 10:24:17 +0900

    feat: Add hierarchical docs structure (Getting Started, Guides)

--- .source/browser.ts
+++ .source/browser.ts
@@ -7,6 +7,6 @@ const create = browser<typeof Config, import("fumadocs-mdx/runtime/types").Inter
   }
 }>();
 const browserCollections = {
-  docs: create.doc("docs", {"index.mdx": () => import("../content/docs/index.mdx?collection=docs"), }),
+  docs: create.doc("docs", {"getting-started/index.mdx": () => import("../content/docs/getting-started/index.mdx?collection=docs"), "getting-started/installation.mdx": () => import("../content/docs/getting-started/installation.mdx?collection=docs"), "guides/advanced-usage.mdx": () => import("../content/docs/guides/advanced-usage.mdx?collection=docs"), }),
 };
 export default browserCollections;
--- .source/server.ts
+++ .source/server.ts
@@ -1,5 +1,9 @@
 // @ts-nocheck
-import * as __fd_glob_0 from "../content/docs/index.mdx?collection=docs"
+import { default as __fd_glob_4 } from "../content/docs/guides/meta.json?collection=meta"
+import { default as __fd_glob_3 } from "../content/docs/getting-started/meta.json?collection=meta"
+import * as __fd_glob_2 from "../content/docs/guides/advanced-usage.mdx?collection=docs"
+import * as __fd_glob_1 from "../content/docs/getting-started/installation.mdx?collection=docs"
+import * as __fd_glob_0 from "../content/docs/getting-started/index.mdx?collection=docs"
 import { server } from 'fumadocs-mdx/runtime/server';
 import type * as Config from '../source.config';
 
@@ -8,6 +12,6 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.doc("docs", "content/docs", {"index.mdx": __fd_glob_0, });
+export const docs = await create.doc("docs", "content/docs", {"getting-started/index.mdx": __fd_glob_0, "getting-started/installation.mdx": __fd_glob_1, "guides/advanced-usage.mdx": __fd_glob_2, });
 
-export const meta = await create.meta("meta", "content/docs", {});
+export const meta = await create.meta("meta", "content/docs", {"getting-started/meta.json": __fd_glob_3, "guides/meta.json": __fd_glob_4, });
